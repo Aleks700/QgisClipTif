74bee7fd5d93b038b0923b40db8a34f83e980156
Alekstok97 <alekstok97@mail.ru>
2022-09-16 15:08:22 +0600

    working

---
+++ FindAll.py
@@ -0,0 +1,20 @@
+import os
+def find_all(name, path):
+    result = []
+    for root, dirs, files in os.walk(path):
+        if name in files:
+            result.append(os.path.join(root, name))
+    return result
+
+
+pathToFind = r'E:\Для снимков'
+name=r'IMAGERY.tif'
+matchup = find_all(name,pathToFind)
+# print(matchup[0],'\n')
+# print(matchup[1],'\n')
+# print(matchup[2],'\n')
+# print(matchup[3],'\n')
+for x in matchup:
+    print(x,'\n')
+
+print( len(matchup))

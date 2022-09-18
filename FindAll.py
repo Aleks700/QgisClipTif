8af1fc42af59dd6f74e1731c5ae0a59936656825
Aleks <alekstok97@mail.ru>
2022-09-19 00:32:43 +0600

    working

--- FindAll.py
+++ FindAll.py
@@ -20,3 +20,33 @@ for x in sorted(matchup):
 
 
 print( len(matchup))
+
+
+
+image_location = os.path.join(QgsProject.instance().homePath(), "render.png")
+
+vlayer = iface.activeLayer()
+settings = QgsMapSettings()
+settings.setLayers([vlayer])
+settings.setBackgroundColor(QColor(255, 255, 255))
+settings.setOutputSize(QSize(800, 600))
+settings.setExtent(vlayer.extent())
+
+render = QgsMapRendererParallelJob(settings)
+
+def finished():
+    img = render.renderedImage()
+    # save the image; e.g. img.save("/Users/myuser/render.png","png")
+    img.save(image_location, "png")
+
+render.finished.connect(finished)
+
+# Start the rendering
+render.start()
+
+# The following loop is not normally required, we
+# are using it here because this is a standalone example.
+from qgis.PyQt.QtCore import QEventLoop
+loop = QEventLoop()
+render.finished.connect(loop.quit)
+loop.exec_()
